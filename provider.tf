f031672234f2d03f6654e155ef702bb943507821
shaheenasultana <93178846+shaheenasultana@users.noreply.github.com>
2022-05-05 16:05:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,27 @@
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_file = "/Users/srupanagudi/.aws/credentials"
+  profile = "testing"
 }
+
+
+
+
+#terraform {
+ #   backend "s3" {
+  #  bucket = "testing-s3-with-terraform56"
+   # key    = "C:/Users/srupanagudi/Desktop/practice1/shaheena/terraform.tfstate"
+   # region = "us-east-1"
+    #}
+#}
+
+terraform {
+  cloud {
+    organization = "NOO"
+
+    workspaces {
+      name = "shaheena"
+    }
+  }
+}
+
